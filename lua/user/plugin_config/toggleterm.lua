a37ad6834ffb10ed00fc12f7c2d08fbe73f6ccd1
adityaanilshukla <adityaanilsindhunath@gmailcom>
2025-08-27 02:20:30 +0800

    make toggleterm open in a tab instead of float

--- lua/user/plugin_config/toggleterm.lua
+++ lua/user/plugin_config/toggleterm.lua
@@ -1,9 +1,10 @@
 local ok, toggleterm = pcall(require, "toggleterm")
-if not ok then return end
+if not ok then
+	return
+end
 toggleterm.setup({
-  direction = "float",
-  open_mapping = [[<F7>]],
-  float_opts = { border = "single", width = 170, height = 45 },
-  close_on_exit = true,
-  shade_terminals = true,
+	direction = "tab", -- opens in a new tab, full screen
+	open_mapping = [[<F7>]],
+	close_on_exit = true,
+	shade_terminals = true,
 })
